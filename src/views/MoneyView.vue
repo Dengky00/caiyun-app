8de1548dcb243361eaf01c952af0d8f0531e8767
Dengky00 <1605980369@qq.com>
2023-06-10 15:09:11 +0800

    feature:MoneyView页面底部添加Nav

--- src/views/MoneyView.vue
+++ src/views/MoneyView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">Money.vue</div>
     <NavBottom />
   </div>
 </template>
@@ -8,10 +8,18 @@
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({
-    
-});
+export default Vue.extend({});
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  flex-grow: 1;
+  overflow: auto;
+}
 </style>
